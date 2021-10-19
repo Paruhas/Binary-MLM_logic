307977663ea777481d1a9f5074b7361ef0fdfac8
Paruhas <bayulgang@hotmail.com>
2021-10-19 12:28:23 +0700

    delete oldPlaceUser Fn

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,6 +5,5 @@ const userController = require("../controllers/userController");
 userRouter.get("/:userId", userController.getUserById);
 userRouter.post("/register", userController.register);
 userRouter.post("/place", userController.placeUser);
-// userRouter.post("/:userId/place", userController.placeUser);
 
 module.exports = userRouter;
